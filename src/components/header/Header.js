79a659145c68b3ce2bc3dd9f301faa4adc1b8b9b
Kenneth Brook <kenneth.brook@hotmail.com>
2021-12-18 17:15:11 -0500

    styled header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,9 +3,17 @@ import React from "react";
 function Header(props) {
     const doit = props.passData[0];
     return (
-        <div>
-            <h1>Welcome to the social conection hub of:</h1>
-            <h1>{doit.fName} {doit.lName}</h1>
+        <div className="header">
+            <div className="one">
+                <div className="two">
+                    <div className="three">
+                        <div className="four">
+                            <h1>Welcome to the social conection hub of:</h1>
+                            <h1>{doit.fName} {doit.lName}</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
